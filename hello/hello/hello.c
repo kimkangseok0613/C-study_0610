8724b6e008a569904d32bdfa14fa04eb5d988f73
kimkangseok0613 <ks0006131@gmail.com>
2024-06-11 17:45:33 +0900

    (2024.06.11) 컴파일과 주석 학습

---
+++ hello/hello/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int Main(void)
+{
+	printf("Hello Study! \n 안녕히 가세요");//\n 이스케이프문자
+	printf("안녕히 가세요");
+	return 0;
+}
---
+++ hello/hello/컴파일러.c
@@ -0,0 +1,12 @@
+#include <stdio.h>//standard input output(표준 입출력)
+
+int main() //main=프로그램의 시작부터 끝
+{
+	printf("☆★★★★★★★★★☆ \n");
+	printf("☆☆★★★★★★★☆☆ \n");
+	printf("☆☆☆★★★★★☆☆☆ \n");
+	printf("☆☆☆☆★★★☆☆☆☆ \n");
+	printf("☆☆☆☆☆★☆☆☆☆☆ \n");
+
+	return 0;
+}
