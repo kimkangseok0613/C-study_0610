69d1284836af0bdfb1b90d9798705af732d5a183
kimkangseok0613 <ks0006131@gmail.com>
2024-07-03 17:49:07 +0900

    (2024.07.03) 별피하기 게임 업그레이드

--- hello/Avoid_Star/main.c
+++ hello/Avoid_Star/main.c
@@ -3,17 +3,55 @@
 #include <stdbool.h>
 #include <time.h>
 #include <stdlib.h>
+#include <conio.h>		// consloe in out	
 
-#define MAX 10	// 전처리기로 MAX 숫자에 값을 10으로 전부 치환하는 코드 (컴파일 시점에)
+#define MAX 30	// 전처리기로 MAX 숫자에 값을 10으로 전부 치환하는 코드 (컴파일 시점에)
+#define BG 15
+
+void StartMenu(void)
+{
+	while (true)
+	{
+		Clear();
+		GotoXY(10, 9);
+		printf("별피하기");
+		GotoXY(5, 20);
+		printf("please press any key");
+
+		if (_kbhit())
+			break;
+	}
+}
+
+void Bomb(void)
+{
+	while (true)
+	{
+		if (GetAsyncKeyState(VK_CONTROL) & 8001)
+			Clear;
+	}
+}
+void GameOver(void)
+{
+	int temp = 0;
+	printf("GAME OVER\n");
+	printf("게임을 종료하려면 아무키를 입력하고 ENTER키를 쳐주세요.\n");
+	scanf_s("%d", &temp);
+}
 
 int main()
 {
+	SetTitle("별피하기_김강석");
+	SetColor(BG,0);
 	SetConsoleSize(30, 30);
 	SetConsoleCursorVisibility(0);
+
+	int score = 0;
 
 	int x = 14, y = 28;
 	int bx = 0, by = 0;
 	bool bullet = false;
+	bool IsLve = true;	// 플레이어가 현재 죽지 않음을 나타냄.
 
 #if false
 	int ex = 0, ey = 0;
@@ -24,10 +62,16 @@ int main()
 	bool enemy[MAX] = { false };
 	srand(time(NULL));
 
+	StartMenu();
+
 #if true
 	while (true)
 	{
 		Clear();
+
+		GotoXY(0, 0);
+		printf("SCORE : %d", score);
+		score++;
 #if false
 		if (!enemy)
 		{
@@ -42,7 +86,7 @@ int main()
 			if (!enemy[i])
 			{
 				ex[i] = (rand() % 15) * 2;
-				ey[i] = 0;
+				ey[i] = 1;
 				enemy[i] = true;
 				break;
 			}
@@ -52,13 +96,13 @@ int main()
 
 		if (GetAsyncKeyState(VK_LEFT) & 8001)
 		{
-			if (x < 1)x = 1;
-			x--;	//
+			if (x < 2)x = 2;
+			x-=2;	
 		}
 		else if (GetAsyncKeyState(VK_RIGHT) & 8001)
 		{
-			if (x > 27) x = 27;
-			x++;
+			if (x > 26) x = 26;
+			x+=2;
 		}
 		else if (GetAsyncKeyState(VK_UP) & 8001)
 		{
@@ -71,8 +115,6 @@ int main()
 			y++;
 		}
 #endif
-		GotoXY(x, y);
-		printf("▲");
 #if true
 		if (GetAsyncKeyState(VK_SPACE) & 8001)
 		
@@ -111,15 +153,46 @@ int main()
 		{
 			if (enemy[i])
 			{
+				SetColor(BG,12);
 				GotoXY(ex[i], ey[i]);
 				printf("☆");
 				ey[i]++;
 
+				//bx, by 총알과 별의 좌표가 같으면 bool값 false
+				if (ey[i] == by && ex[i]==bx && bullet)
+				{
+					enemy[i] = false;
+					bullet = false;
+				}
+
+				if (x == ex[i] && y == ey[i])
+				{
+					if (IsLve)// 플레이어가 죽었을때 작동하는 로직
+					{
+						IsLve = false;
+					}
+				}
+
 				if (ey[i] > 28)
 					enemy[i] = false;
 			}
+			
 		}
-
-		Sleep(100);
+		//플레이어가 살아있을때 현재 플레이어의 위치에 ▲출력해주는 코드
+		if (IsLve)
+		{
+			SetColor(BG, 12);
+			GotoXY(x, y);
+			printf("▲");
+		}
+		else
+		{
+			GotoXY(x, y);
+			printf("@");
+		
+			break;
+		}
+		Sleep(100);	//숫자만큼 기다렸다가 다시 실행한다.
 	}
+	GameOver();
 }
